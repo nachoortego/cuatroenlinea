63ef097ef44408716fc76171319fb769cabd8cd3
Ignacio Ortego <ignacioortego110@gmail.com>
2022-06-30 17:18:26 -0300

    New Test for vertical and horizontal overflow

---
+++ tests/Feature/OverFlowTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class OverFlowTest extends TestCase
+{
+    public function test_horizontal_overflow() {
+        $number = $this->get('/jugar/12345678'); // Number 8 is not considered on the board
+        $number->assertStatus(500);    // Should receive a 500
+    }
+
+    public function test_vertical_overflow() {
+        $number = $this->get('/jugar/1111111'); // Seven 1's should result in getting out of the board
+        $number->assertStatus(500);    // Should receive a 500
+    }
+}
