00af42fcd1e94f5aeb1b1a4a0afe26af6058aa00
Ignacio Ortego <ignacioortego110@gmail.com>
2022-06-30 17:18:04 -0300

    Minimal corrections

--- tests/Feature/BoxCountTest.php
+++ tests/Feature/BoxCountTest.php
@@ -10,19 +10,19 @@ class BoxCountTest extends TestCase
     public function test_count_white_box()
     {
         $html = file_get_contents('http://dagosproject.ddev.site:800/jugar/234234212137');
-
-        $this->assertTrue(substr_count($html,'class="bg-gray-200 text-center mx-1 mt-1 w-24 h-24"') == 30);
+        
+        $this->assertTrue(substr_count($html,'class="bg-gray-200') == 30);
     }
     public function test_count_red_box()
     {
         $html = file_get_contents('http://dagosproject.ddev.site:800/jugar/234234212137');
 
-        $this->assertTrue(substr_count($html,'class="bg-red-500 text-center mx-1 mt-1 w-24 h-24"') == 6);
+        $this->assertTrue(substr_count($html,'class="bg-red-500') == 6);
     }
     public function test_count_blue_box()
     {
         $html = file_get_contents('http://dagosproject.ddev.site:800/jugar/234234212137');
 
-        $this->assertTrue(substr_count($html,'class="bg-sky-500 text-center mx-1 mt-1 w-24 h-24"') == 6);
+        $this->assertTrue(substr_count($html,'class="bg-sky-500') == 6);
     }
 }
