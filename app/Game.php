d3f93005052cad56f6547fcee4497096e17bcde9
Ignacio Ortego <ignacioortego110@gmail.com>
2022-08-30 23:11:23 -0300

    horizontal and vertical checks

--- app/Game.php
+++ app/Game.php
@@ -5,10 +5,10 @@ namespace App;
 include 'Board.php';
 
 interface gameInterface {
-    public function horizontal(Board $board) : bool; // Reads if there is a horizonal 4-in-row in the board
-    public function vertical(Board $board) : bool; // Reads if there is a vertical 4-in-row in the board
-    public function diagonal(Board $board) : bool; // Reads if there is a diagonal 4-in-row in the board
-    public function winner(Board $board); // Sets the winner and ends the game
+    public function horizontal() : bool; // Reads if there is a horizonal 4-in-row in the board
+    public function vertical() : bool; // Reads if there is a vertical 4-in-row in the board
+    public function diagonal() : bool; // Reads if there is a diagonal 4-in-row in the board
+    public function winner(string $player); // Sets the winner and ends the game
 }
 
 class Game implements gameInterface {
@@ -18,15 +18,55 @@ class Game implements gameInterface {
         $this->board = $newBoard->board;
     }
 
-    public function horizontal($board) : bool {
-        return TRUE;
+    public function horizontal() : bool {
+        $red = 0;
+        $blue = 0;
+
+        for($y = ($this->board)->getY()-1; $y >= 0; $y--){
+            for($x = 0; $x < ($this->board)->getX(); $x++){
+                if(strcmp(($this->board[$x][$y])->getColor(), "🟥") == 0 /*R*/) {
+                    $red++;
+                    $blue == 0;
+                    if($red == 4)
+                        return TRUE;
+                }
+                if(strcmp(($this->board[$x][$y])->getColor(), "🟦") == 0 /*B*/) {
+                    $blue++;
+                    $red = 0;
+                    if($blue == 3)
+                        return TRUE;
+                }
+            }
+        }
+
+        return FALSE;    
     }
 
-    public function vertical($board) : bool {
-        return TRUE;
+    public function vertical() : bool {
+        $red = 0;
+        $blue = 0;
+
+        for($x = 0; $x < ($this->board)->getX(); $x++){
+            for($y = ($this->board)->getY()-1; $y >= 0; $y--){
+                if(strcmp(($this->board[$x][$y])->getColor(), "🟥") == 0 /*R*/) {
+                    $red++;
+                    $blue == 0;
+                    if($red == 4)
+                        return TRUE;
+                }
+                if(strcmp(($this->board[$x][$y])->getColor(), "🟦") == 0 /*B*/) {
+                    $blue++;
+                    $red = 0;
+                    if($blue == 4)
+                        return TRUE;
+                }
+            }
+        }
+        
+        return FALSE;    
     }
 
-    public function diagonal($board) : bool {
+    public function diagonal() : bool {
         return TRUE;
     }
 
